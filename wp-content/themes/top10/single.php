e4fcf8351223c82d790d4c57a738910a47d7a759
Tran Cong <trancong.joomla@gmail.com>
2018-10-26 19:56:51 +0700

    add ads top and right post

--- wp-content/themes/top10/single.php
+++ wp-content/themes/top10/single.php
@@ -16,6 +16,58 @@ $td_mod_single = new td_module_single($post);
         <div class="td-crumb-container"><?php echo td_page_generator::get_single_breadcrumbs($td_mod_single->title); ?></div>
 
         <div class="td-pb-row">
+
+            <!-- ads here -->
+            <div class="td-pb-span12">
+            <?php
+            $image_top_1 = get_field('image_top_1');
+            $image_top_2 = get_field('image_top_2');
+            $image_top_3 = get_field('image_top_3');
+            $image_top_4 = get_field('image_top_4');
+            ?>
+            <div class="ads_top">
+            <?php if($image_top_1 or $image_top_2 or $image_top_3 or $image_top_4): ?>
+                <ul>
+                    <?php if($image_top_1): ?>
+                    <li><img src="<?php echo $image_top_1['url']; ?>" alt="<?php echo $image_top_1['alt']; ?>"  /></li>
+                    <?php endif; ?>
+
+                    <?php if($image_top_2): ?>
+                        <li><img src="<?php echo $image_top_2['url']; ?>" alt="<?php echo $image_top_2['alt']; ?>"  /></li>
+                    <?php endif; ?>
+
+                    <?php if($image_top_3): ?>
+                        <li><img src="<?php echo $image_top_3['url']; ?>" alt="<?php echo $image_top_3['alt']; ?>"  /></li>
+                    <?php endif; ?>
+
+                    <?php if($image_top_4): ?>
+                        <li><img src="<?php echo $image_top_4['url']; ?>" alt="<?php echo $image_top_4['alt']; ?>"  /></li>
+                    <?php endif; ?>
+                </ul>
+            <?php else: ?>
+                <ul>
+                    <li><a href="<?php echo site_url('/lien-he'); ?>">
+                            <img src="<?php echo get_template_directory_uri().'/images/ads/lien-he-quang-cao-top.jpg' ?>"  />
+                        </a>
+                    </li>
+                    <li><a href="<?php echo site_url('/lien-he'); ?>">
+                            <img src="<?php echo get_template_directory_uri().'/images/ads/lien-he-quang-cao-top.jpg' ?>"  />
+                        </a>
+                    </li>
+                    <li><a href="<?php echo site_url('/lien-he'); ?>">
+                            <img src="<?php echo get_template_directory_uri().'/images/ads/lien-he-quang-cao-top.jpg' ?>"  />
+                        </a>
+                    </li>
+                    <li><a href="<?php echo site_url('/lien-he'); ?>">
+                            <img src="<?php echo get_template_directory_uri().'/images/ads/lien-he-quang-cao-top.jpg' ?>"  />
+                        </a>
+                    </li>
+                </ul>
+            <?php endif; ?>
+            </div>
+            </div>
+            <!-- end ads here -->
+
             <?php
 
             //the default template
@@ -35,16 +87,23 @@ $td_mod_single = new td_module_single($post);
 
                                  <?php $image_1 = get_field('image_1'); ?>
                                  <?php $image_2 = get_field('image_2'); ?>
-                                 <?php if($image_1): ?>
-                                 <div class="image_1">
-                                    <img src="<?php echo $image_1['url']; ?>" alt="<?php echo $image_1['alt']; ?>" />
-                                 </div>
-                                 <?php endif; ?>
+                                 <?php if($image_1 or $image_2): ?>
 
-                                <?php if($image_2): ?>
-                                    <div class="image_2">
-                                        <img src="<?php echo $image_2['url']; ?>" alt="<?php echo $image_2['alt']; ?>" />
-                                    </div>
+                                     <?php if($image_1): ?>
+                                     <div class="image_1">
+                                        <img src="<?php echo $image_1['url']; ?>" alt="<?php echo $image_1['alt']; ?>" />
+                                     </div>
+                                     <?php endif; ?>
+                                     <?php if($image_2): ?>
+                                        <div class="image_2">
+                                            <img src="<?php echo $image_2['url']; ?>" alt="<?php echo $image_2['alt']; ?>" />
+                                        </div>
+                                     <?php endif; ?>
+
+                                 <?php else: ?>
+                                <div class="contact_for_ads_right">
+                                    <a href="<?php echo site_url('/lien-he'); ?>"><img src="<?php echo get_template_directory_uri().'/images/ads/lien-he-quang-cao.jpg' ?>"  /></a>
+                                </div>
                                 <?php endif; ?>
 
                                 <?php get_sidebar(); ?>
